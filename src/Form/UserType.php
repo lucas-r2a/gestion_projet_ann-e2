d34c35eb024a8cc72bdbfc8c53c2f11d1529c515
lucas-r2a <lucasecole2@gmail.com>
2025-11-18 12:05:40 +0100

    Styliser tous les templates twig et ajout des roles (admin, user et manager) dans le UserType

--- src/Form/UserType.php
+++ src/Form/UserType.php
@@ -4,6 +4,7 @@ namespace App\Form;
 
 use App\Entity\User;
 use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\ChoiceType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
 
@@ -13,7 +14,16 @@ class UserType extends AbstractType
     {
         $builder
             ->add('email')
-            ->add('roles')
+            ->add('roles', ChoiceType::class, [
+                'choices' => [
+                    'Utilisateur' => 'ROLE_USER',
+                    'Manager' => 'ROLE_MANAGER',
+                    'Admin' => 'ROLE_ADMIN',
+                ],
+                'multiple' => true,
+                'expanded' => false, // select multiple
+                'label' => 'Rôles',
+            ])
             ->add('password')
             ->add('name')
         ;
